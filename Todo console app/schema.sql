044ded78edfe10970b19d05e50bb2a9b5346cb59
Parah <122792967+ParahDime@users.noreply.github.com>
2026-02-06 13:17:52 +0000

    Created tables for schema.sql, to do add populating data to seed.sql

--- Todo console app/schema.sql
+++ Todo console app/schema.sql
@@ -1,1 +1,19 @@
-﻿
+﻿--Table for user information
+CREATE TABLE Users (
+	id INTEGER PRIMARY KEY,
+	userID TEXT NOT NULL UNIQUE,
+	passwrd_hash REAL NOT NULL
+
+);
+
+--Table for actions on the ToDo list
+CREATE TABLE ActionsToDo (
+	id INTEGER NOT NULL,
+	userID INTEGER NOT NULL,
+	title TEXT NOT NULL,
+	description TEXT NULL,
+	is_complete INTEGER NOT NULL DEFAULT 0,
+	time_create INTEGER NOT NULL DEFAULT CURRENT_TIMESTAMP --could be text?
+
+	FOREIGN KEY (userID) REFERENCES Users(id) --end of table foreign key
+);
